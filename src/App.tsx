793fa8a72c7fb22bd9110e2898ffa580f66bf635
howa870 <65369624+howa870@users.noreply.github.com>
2026-03-23 07:01:38 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
+import { RouterProvider } from 'react-router';
+import { router } from './routes';
+
 export default function App() {
-  return (
-    <div style={{textAlign:"center", padding:"50px"}}>
-      <h1>تفنات ودواوين فخمة</h1>
-      <p>موقعك اشتغل بنجاح 🔥</p>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }

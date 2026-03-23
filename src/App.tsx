f0e9f4c8eb36b93c52886b4ee32d12f824b3a04f
howa870 <65369624+howa870@users.noreply.github.com>
2026-03-23 05:16:22 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{textAlign:"center", padding:"50px"}}>
+      <h1>تفنات ودواوين فخمة</h1>
+      <p>موقعك اشتغل بنجاح 🔥</p>
+    </div>
+  )
+}
